86c278a56441f2e4ccf6c4cf4b6190bd52514559
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-09-05 20:00:59 +0200

    Add trainings model, controller ,routes, etc.

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
-const mongoose = require('./mongoose');
 const models = require('./models');
+const mongoose = require('./mongoose');
 
 module.exports.mongoose = mongoose;
 
+module.exports.Trainings = models.trainings;
 module.exports.Users = models.users;
